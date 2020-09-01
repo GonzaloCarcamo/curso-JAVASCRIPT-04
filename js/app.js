1ed5bca53a71465dd29eba0c11771e3bed3a4a9a
goncmodev <gon.cmo.dev@gmail.com>
2020-09-01 21:07:46 +0200

    Generar un select en años

--- js/app.js
+++ js/app.js
@@ -1,16 +1,24 @@
 //Variables
 const resultado = document.querySelector('#resultado');
+const year = document.querySelector('#year');
+
+const max = new Date().getFullYear(); // Año máximo
+const min = max - 10; // año mínimo
 
 
 //Eventos
 
 document.addEventListener('DOMContentLoaded', () => { //Una vez cargado el HTML, se inicia la función mostrarAutos();
     mostrarAutos();
+
+    // Lenar las opciones de años
+    llenarSelect();
 })
 
 
 //Funciones
 
+// Muestra el listado de autos
 function mostrarAutos() {
     autos.forEach(auto => {
 
@@ -26,6 +34,16 @@ function mostrarAutos() {
     }) 
 }
 
+// Genera los años
+function llenarSelect() {
+    
+    for( let i = max; i >= min; i-- ) {
+        const opcion = document.createElement('option');
+        opcion.value = i;
+        opcion.textContent = i;
+        year.appendChild(opcion); // Agrega las opciones de años al select
+    }
+}
 
 
 
@@ -43,7 +61,7 @@ Flujo de la aplicación:
 1. Al cargar el HTML, se inicializa la función mostrarAutos().
 2. Esta función itera el array de objetos "autos". 
 3. Se genera un <p> por cada elemento y se muestra en el HTML dentro del <div> con id="resultado"
-
+4. Luego de generan los años dentro del select a través de la función llenarSelect().
 
 
 */
